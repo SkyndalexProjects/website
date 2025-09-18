4efd05521d44e761f744e72df6bf0a847f8a9cbb
KorrumzPL <korrumzprivate@gmail.com>
2025-09-18 20:27:35 +0200

    fix: adapter

--- svelte.config.js
+++ svelte.config.js
@@ -7,22 +7,15 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
-	// kit: {
-	// 	// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-	// 	// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-	// 	// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-	// 	adapter: adapter({
-	// 		pages: "build",
-	// 		assets: "build",
-	// 		fallback: undefined,
-	// 		precompress: false,
-	// 		strict: true
-	// 	})
-	// }
 	kit: {
 		adapter: adapter({
 			fallback: "404.html"
 		}),
+		pages: 'build',
+		assets: 'build',
+		fallback: undefined,
+		precompress: false,
+		strict: true,
 		paths: {
 			base: process.argv.includes("dev") ? "" : process.env.BASE_PATH
 		},
@@ -32,7 +25,6 @@ const config = {
 					console.warn(`Prerendering failed for ${path} with status ${status}`);
 					return;
 				}
-				// For other errors, throw as usual
 				throw new Error(`${status} error at ${path}`);
 			}
 		}
