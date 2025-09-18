6d8b56a02239afeacecc383a080fb6fabacebd78
KorrumzPL <korrumzprivate@gmail.com>
2025-09-18 20:09:20 +0200

    fix: github actions

--- svelte.config.js
+++ svelte.config.js
@@ -7,17 +7,25 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
+	// kit: {
+	// 	// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+	// 	// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+	// 	// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+	// 	adapter: adapter({
+	// 		pages: "build",
+	// 		assets: "build",
+	// 		fallback: undefined,
+	// 		precompress: false,
+	// 		strict: true
+	// 	})
+	// }
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			pages: "build",
-			assets: "build",
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+			fallback: "404.html"
+		}),
+		paths: {
+			base: process.argv.includes("dev") ? "" : process.env.BASE_PATH
+		}
 	}
 };
 
